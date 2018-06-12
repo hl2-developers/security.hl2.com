d5ead67a39ea1f04c2eae16066bb447024080f48
kevinpollet <pollet.kevin@gmail.com>
2018-06-12 11:38:20 +0200

    Rework gunshot detection section

--- _assets/js/default-layout.js
+++ _assets/js/default-layout.js
@@ -7,5 +7,24 @@
     $(".navbar-nav li a").click(function(event) {
       $(".navbar-collapse").collapse('hide');
     });
+
+    /**
+     * Initialize youtube player
+     */
+    const player = new Plyr('#player', {
+      autoplay: true,
+      controls: [
+        'play',
+        'progress',
+        'current-time',
+        'captions',
+        'pip',
+        'airplay',
+        'fullscreen'
+      ],
+      loop: {
+        active: true
+      }
+    });
   });
 })();
